632686aa316efab66894df1fc13ca858cff20adf
Mareike Thieben <mthieben@posteo.de>
2020-07-05 12:48:09 +0200

    updates

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return "Hello, SmartNinja!"
 
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
 
 if __name__ == '__main__':
     app.run()
